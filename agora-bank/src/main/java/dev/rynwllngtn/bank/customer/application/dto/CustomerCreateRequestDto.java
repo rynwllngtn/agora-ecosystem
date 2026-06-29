3f1a7f552601185b708909e65fa3fd260b85da1c
Ryan Wellington <rynwllngtn@gmail.com>
2026-06-29 18:01:09 -0300

    feat(api/bank/customer): implementa handler de exceções e validações
    - (infrastructure/handler): implementa handler exceções customizadas
    - (application/dto): adiciona validações com mensagens explicativas

--- agora-bank/src/main/java/dev/rynwllngtn/bank/customer/application/dto/CustomerCreateRequestDto.java
+++ agora-bank/src/main/java/dev/rynwllngtn/bank/customer/application/dto/CustomerCreateRequestDto.java
@@ -1,9 +1,22 @@
 package dev.rynwllngtn.bank.customer.application.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
+
 import java.util.UUID;
 
 public record CustomerCreateRequestDto(
+        @NotNull(message = "O identityId não pode ser nulo")
         UUID identityId,
+        @NotBlank(message = "O cpf não pode ser vazio")
+        @CPF(message = "O cpf deve ser válido")
         String cpf,
+        @NotBlank(message = "O email não pode ser vazio")
+        @Email(
+                regexp = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$",
+                message = "O email deve ser válido"
+        )
         String email
 ) {}
---
+++ agora-bank/src/main/java/dev/rynwllngtn/bank/customer/infrastructure/handler/ApplicationExceptionHandler.java
@@ -0,0 +1,22 @@
+package dev.rynwllngtn.bank.customer.infrastructure.handler;
+
+import dev.rynwllngtn.bank.customer.application.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler(value = ResourceNotFoundException.class)
+    public ProblemDetail resourceNotFound(ResourceNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                                                                       e.getMessage());
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+}
